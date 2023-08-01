fe126159f19b5b58c953dcd4bb9c2a42c7d8796a
Elle Mundy <exchgr@icloud.com>
2023-08-01 17:51:39 -0400

    Add RSS feed for all articles

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,21 @@ const markdownIt = require("markdown-it")({
 	html: true,
 	typographer: true
 })
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+const nunjucks = require("nunjucks")
 
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addFilter('markdown', body => markdownIt.render(body))
+	eleventyConfig.addFilter('interpolate', function(body, article) {
+		return nunjucks.renderString(body, { article })
+	})
 	eleventyConfig.setLibrary("md", markdownIt)
 
 	// include assets
 	eleventyConfig.addPassthroughCopy({"src/styles": "styles"})
 	eleventyConfig.addPassthroughCopy({"src/fonts": "fonts"})
+
+	eleventyConfig.addPlugin(pluginRss);
 
 	return {
 		dir: {
