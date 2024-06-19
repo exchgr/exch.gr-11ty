077dc9566203b9ccee689dcfdb18f136270d5d21
Elle Mundy <exchgr@icloud.com>
2024-06-19 17:41:02 -0400

    Change font-display to "block" to mostly avoid fout, but only small foit

--- src/views/_data/googleFonts.js
+++ src/views/_data/googleFonts.js
@@ -2,7 +2,7 @@ const fetch = require("@11ty/eleventy-fetch");
 
 module.exports = async () => {
 	try {
-		return await fetch("https://fonts.googleapis.com/css2?family=Mrs+Sheppards&family=Playfair+Display:wght@800&display=swap", {
+		return await fetch("https://fonts.googleapis.com/css2?family=Mrs+Sheppards&family=Playfair+Display:wght@800&display=block", {
 			duration: "1y",
 			type: "text",
 		})
