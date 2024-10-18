32689dac009fdb7e127529ccf6a0c94c471964bc
Elle Mundy <exchgr@icloud.com>
2024-10-18 12:43:53 -0400

    Match clickops drift: github deployment (sometimes!!)

--- terraform/cloudflare_pages.tf
+++ terraform/cloudflare_pages.tf
@@ -19,8 +19,23 @@ resource "cloudflare_pages_project" "cloudflare_pages_project" {
 				"STRAPI_HOST"     = data.external.env.result["STRAPI_HOST"]
 				"STRAPI_PORT"     = data.external.env.result["STRAPI_PORT"]
 				"STRAPI_PROTOCOL" = data.external.env.result["STRAPI_PROTOCOL"]
+				"STRAPI_FETCH_INTERVAL" = data.external.env.result["STRAPI_FETCH_INTERVAL"]
 			}
 			fail_open = true
+		}
+	}
+
+	source {
+		type = "github"
+
+		config {
+			owner                   = data.external.env.result["GITHUB_REPOSITORY_OWNER"]
+			production_deployment_enabled = false
+			preview_branch_includes = [
+				"*",
+			]
+			production_branch       = data.external.env.result["GITHUB_REF_NAME"]
+			repo_name               = data.external.env.result["GITHUB_REPOSITORY_NAME"]
 		}
 	}
 }
--- terraform/cloudflare_record.tf
+++ terraform/cloudflare_record.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "apex" {
   name    = "exch.gr"
-  proxied = false
-  ttl     = 60
+  proxied = true
+  ttl     = 1
   type    = "CNAME"
   value   = "exch-gr-11ty.pages.dev"
 	zone_id = data.external.env.result["CLOUDFLARE_ZONE_ID"]
